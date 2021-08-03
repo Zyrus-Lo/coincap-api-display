910d8c2ddee0381d9be3054b7f02c5f5127b4962
Zyrus-Lo <zyrus.lo@tryncatchstudio.com>
2021-08-04 00:28:28 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,37 @@
 
 import { useState, useEffect, useRef } from 'react'
 import Table from '../components/share/Table'
-import { Container, Input, Heading, Grid, Button, Box} from 'theme-ui'
+import { Container, Input, Heading, Grid, Button, Box, Spinner} from 'theme-ui'
+
+import {
+  useQuery,
+  useMutation,
+} from 'react-query'
+
 
 export default function Home() {
   
   const inputEl = useRef(null);
-  const [initData, setInitData] = useState([]);
   const [tableData, setTableData] = useState([]);
 
-  useEffect(async()=>{
-    let res = await fetch("https://api.coincap.io/v2/assets?limit=20").then((res) => res.json())
-    setInitData(res.data);
-  },[]);
+ 
+
+  const { isLoading, data: initData, isSuccess } = useQuery('initData', () =>
+     fetch('https://api.coincap.io/v2/assets?limit=20').then(res =>
+       res.json()
+     ),
+   )
+
+   useEffect(()=> { 
+    setTableData(initData?.data)
+   },[initData])
+ 
+   if (isLoading) return 'Loading...' 
 
   const onSearchClick = () => {
     let inputText = inputEl.current.value.toLowerCase();
     if(inputText){
-      let filterResult = initData.filter(obj => {
+      let filterResult = tableData.filter(obj => {
         if(obj.symbol.toLowerCase() === inputText || obj.name.toLowerCase() === inputText){
           return obj;
         }
@@ -25,29 +39,33 @@ export default function Home() {
       })
       setTableData(filterResult);
     }else{
-      setTableData([]);
+      setTableData(initData?.data);
     }
   };
 
   const onResetClick = () => {
     inputEl.current.value = '';
-    setTableData(initData);
+    setTableData([]);
   }
-
   return (
     <Container p={4}>
       <Grid columns={[1,3,4]} >
         <Heading as="h1" sx={{textAlign:['center','left']}}>Crypto Market</Heading>
-        <Input rows={1} ref={inputEl} />
+        <Input rows={1} ref={inputEl} placeholder='e.g. BTC'/>
         <Button onClick={onSearchClick} ml={[0,0,2]}>Search</Button>
         <Button onClick={onResetClick} ml={[0,0,2]}>Reset</Button>
       </Grid>
-      <Box my={2} sx={{color: 'red', textAlign:'center'}}> 
+      <Box>
+
         {
-          tableData.length? "" : 'No Record Found'
+          ( !isLoading && tableData.length === 0) ? 'NO' : ''
         }
       </Box>
-      <Table apiData={tableData || initData} />
+      {
+        isLoading ? 
+        <Spinner /> : 
+        <Table apiData={tableData} />
+      }
     </Container>
   )
 }
